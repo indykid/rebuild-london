dd44593778c48d2b96c818427076b17a69269bb5
Jarkyn S <jarkyn.soltobaeva@matchesfashion.com>
2018-10-09 18:29:02 +0100

    Initial

---
+++ js/company.js
@@ -0,0 +1,12 @@
+// $(function() {
+//   $('.carousel').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 400,
+//     fade: true,
+//     cssEase: 'linear',
+//     // slidesToShow: 1,
+//     // centerMode: true,
+//     // variableWidth: true
+//   });
+// });
